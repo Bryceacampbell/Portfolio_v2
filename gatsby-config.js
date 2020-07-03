b3dd2f8ef23c3110a92ec3e05fc1077e995b4060
bryceacampbell <bryceacampbell7@gmail.com>
2020-07-03 13:08:58 -0700

    adding gatsby material ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@brycecampbell`,
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,

44e50232cd54c008598bf9ed3da3076fdfde3985
bryceacampbell <bryceacampbell7@gmail.com>
2020-07-04 15:51:46 -0700

    updating social media links

--- src/components/layout/components/footer/components/Social.js
+++ src/components/layout/components/footer/components/Social.js
@@ -1,38 +1,55 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { Grid, Tooltip } from '@material-ui/core';
 import { Mail, Twitter, LinkedIn, GitHub } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    link: {
+        textDecoration: 'none',
+        color: '#111111'
+    }
+});
 
 const socialList = [
     {
         name: 'Email',
-        icon: <Mail/>,
-        tooltip: 'Click to send me an email'
+        icon: <Mail />,
+        tooltip: 'Click to send me an email',
+        href: '#'
     },
     {
         name: 'Twitter',
         icon: <Twitter />,
-        tooltip: 'Follow me on Twitter'
+        tooltip: 'Follow me on Twitter',
+        href: 'https://twitter.com/BryceACampbell'
     },
     {
         name: 'LinkedIn',
         icon: <LinkedIn />,
-        tooltip: 'Connect with me on LinkedIn'
+        tooltip: 'Connect with me on LinkedIn',
+        href: 'https://linkedin.com/in/bryceacampbell7'
     },
     {
         name: 'GitHub',
         icon: <GitHub />,
-        tooltip: 'Check out my repositories'
+        tooltip: 'Check out my repositories',
+        href: 'https://github.com/Bryceacampbell'
     },
-]
+];
 
 const Social = () => {
+    const classes = useStyles();
+
     return (
         <Grid container alignItems='center' spacing={2}>
-            {socialList.map(({ name, icon, tooltip }) => (
+            {socialList.map(({ name, icon, tooltip, href }) => (
                 <Grid key={name} item xs>
-                    <Tooltip title={tooltip}>
-                        {icon}
-                    </Tooltip>
+                    <Link href={href} target='_blank' className={classes.link}>
+                        <Tooltip title={tooltip}>
+                            {icon}
+                        </Tooltip>
+                    </Link>
                 </Grid>
             ))}
         </Grid>
